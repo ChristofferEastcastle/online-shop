4ceca22e1521b03a2bd881ae887c2e32aef018d2
christoffer <links685@gmail.com>
2022-11-23 00:49:00 +0100

    added docker support as default. Started on README and build scripts

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+mvn package
+SERVICE=discovery-service
+docker build -t $SERVICE -f $SERVICE/Dockerfile $SERVICE
+SERVICE=gateway-service
+docker build -t $SERVICE -f $SERVICE/Dockerfile $SERVICE
+SERVICE=order-service
+docker build -t $SERVICE -f $SERVICE/Dockerfile $SERVICE
+SERVICE=payment-service
+docker build -t $SERVICE -f $SERVICE/Dockerfile $SERVICE
+
+docker-compose --profile online-shop up
